306ab53adf39174ce3792ca2daf2ee573efbb7ba
Ivan <lavlisnkiy.i@mail.ru>
2025-10-30 12:13:35 +0300

    Обновил пути к проекту в конфигурациях Nginx и PM2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   apps: [{
     name: 'whatsappbot',
     script: './server.js',
-    cwd: '/www/bot.capitalmars.com',
+    cwd: '/var/www/u2817882/data/www/bot.capitalmars.com',
     
     // Режим работы
     instances: 1,
@@ -24,8 +24,8 @@ module.exports = {
     min_uptime: '10s',
     
     // Логи
-    error_file: '/www/bot.capitalmars.com/logs/pm2-error.log',
-    out_file: '/www/bot.capitalmars.com/logs/pm2-out.log',
+    error_file: '/var/www/u2817882/data/www/bot.capitalmars.com/logs/pm2-error.log',
+    out_file: '/var/www/u2817882/data/www/bot.capitalmars.com/logs/pm2-out.log',
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
     merge_logs: true,
     
